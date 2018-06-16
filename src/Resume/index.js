773c297737a0779948291cf061f53d957a434493
zlh <17612235899@163.com>
2018-06-16 14:52:04 +0800

    '架构样式搭建'

--- src/Resume/index.js
+++ src/Resume/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './index.less';
-import netWork from '../Services/index';
+import netWork from '../Services/fetch';
 import MyBaseInfo from './../Component/MyBaseInfo/index';
 import MyIntroduce from './../Component/MyIntroduce/index';
 import MyExperience from './../Component/MyExperience/index';
