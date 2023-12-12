da196c096b23e8a3badeb6fc59ad135900735cbb
张凌浩 <wb22@ehualu.com>
2023-12-12 11:33:37 +0800

    修改mock

--- src/MockData/index.js
+++ src/MockData/index.js
@@ -1,225 +1,290 @@
 export default {
-    "data": {
-        baseInfo: {
-            nikeName: 'Walter White',
-            firstName: '张',
-            lastName: '凌浩',
-            nameFootNote: '一个Vue,React技术方向的前端开发者',
-            gender: '男',
-            age: '32',
-            education: '专科',
-            exp: '7年前端开发经验',
-            phone: '17612235899',
-            QQ: '271793819',
-            email: '17612235899@163.com',
-            gitHub: 'https://github.com/linghaozhang',
-            location: '天津'
-        },
-        introduce: {
-            content: [
-                "7年＋的前端开发经验。2年＋的北京React+redux开发经验。参与过内部管理系统开发和电商开发。",
-                "近期独立带队完成大型智慧交通项目",
-                "擅长通用组件库开发，疑难问题公关",
-                "做过小程序开发",
-                "有做过web APP。",
-                "用jest做过前端单测",
-                "经常使用 antd 和 less，喜欢逛掘金，SegmentFault，github等开发者社区。",
-            ],
-            tech: ['react', 'vue', 'redux', 'react-redux', 'dva', 'ES6', 'less', 'antd', 'antd-mobile', 'webpack', 'jquery', 'bootStrap', 'H5', 'git']
-        },
-        experience: [
-            {
-            time: "",
-            title: "北京易华录信息技术股份有限公司",
-            proTitle: "Eloc信控平台",
-            intro: "高级前端开发",
-            description: "主导了前后分离，整体项目重构",
-            tips: [{
-                title: "主要业绩：",
-                content: [
-                    "平台重构100%",
-                    "开发了核心功能组件",
-                    "封装基础工具函数、组件",
-                    "疑难问题公关。",
-                    "通过严酷测试，如期发布版本",
-                ]
-            }],
-            technology: [{
-                title: "技术栈：",
-                content: [
-                    "vue", "vux", "webpack", "less", "ES6"
-                ]
-            }]
-        },
-            {
-            time: "",
-            title: "易商阜极科技股份有限公司",
-            proTitle: "SCRM",
-            intro: "中级前端开发",
-            description: "主要负责搭建基于vue的h5开发环境。",
-            tips: [{
-                title: "主要业绩：",
-                content: [
-                    "使用vue和vux组件库搭建移动端开发环境。",
-                    "封装基础工具函数、组件",
-                    "给组内成员培训并讲解。"
-                ]
-            }],
-            technology: [{
-                title: "技术栈：",
-                content: [
-                    "vue", "vux", "webpack", "less", "ES6"
-                ]
-            }]
-        },
-            {
-                time: "",
-                title: "易商阜极科技股份有限公司",
-                proTitle: "信美电商积分兑换系统",
-                intro: "中级前端开发",
-                description: "主要负责基于react的Pc和webApp的前端开发，系统支持内部积分兑换，支付宝购买等流程。",
-                tips: [{
-                    title: "主要业绩：",
-                    content: [
-                        "自己重构了之前的积分兑换代码，组件化思想开发，是代码可读性大大提高。",
-                        "实现了整个系统的兑换流程的优化。实现前后端分离。",
-                        "给组内成员做了技术分享。"
-                    ]
-                }],
-                technology: [{
-                    title: "技术栈：",
-                    content: [
-                        "react", "react-redux", "antd", "antd-mobile", "webpack", "less", "ES6"
-                    ]
-                }]
-            },
-            {
-                time: "",
-                title: "易商阜极科技股份有限公司",
-                proTitle: "信美保险产品",
-                intro: "中级前端开发",
-                description: "主要负责基于react的信美保险产品的开发,主要流程包括险种价格试算，投被保人信息，健康告知，face++人脸识别。",
-                tips: [{
-                    title: "主要业绩：",
-                    content: [
-                        "期间做了：共创配置中心，海外医疗，高端医疗，交通意外，短期意外等险种",
-                        "在前端架构leader的带领下，优化了配置中心的数据模型，使共创中心实现了多渠道对接。",
-                        "同时在app端重构了家庭成员组件"
-                    ]
-                }],
-                technology: [{
-                    title: "技术栈：",
-                    content: [
-                        "react", "react-redux", "antd", "antd-mobile", "webpack", "less", "ES6"
-                    ]
-                }]
-            },
-            {
-                time: "",
-                title: "易商阜极科技股份有限公司",
-                proTitle: "标准工程",
-                intro: "中级前端开发",
-                description: "主要负责工程内常用方法、组件的封装",
-                tips: [{
-                    title: "主要业绩：",
-                    content: [
-                        "期间封装了基于fetch的异步请求方法",
-                        "基于antd2.x的表单组件封装",
-                        "参与了使用标准工程开发的外部bp系统，企业管理部分"
-                    ]
-                }],
-                technology: [{
-                    title: "技术栈：",
-                    content: [
-                        "react", "react-redux", "antd", "antd-mobile", "webpack", "less", "ES6"
-                    ]
-                }]
-            },
-            {
-                time: "",
-                title: "易商阜极科技股份有限公司",
-                proTitle: "电商保全项目",
-                intro: "中级前端开发",
-                description: "该项目基于已承保的用户，进行保全申请操作",
-                tips: [{
-                    title: "主要业绩：",
-                    content: [
-                        "完成了贷款清偿，减保保全项目",
-                        "使用了face++人脸识别，对用户进行身份鉴定",
-                        "参与了清偿需求分析，与后端共同制定报文"
-                    ]
-                }],
-                technology: [{
-                    title: "技术栈：",
-                    content: [
-                        "react", "react-redux", "antd", "antd-mobile", "webpack", "less", "ES6"
-                    ]
-                }]
-            }
-        ],
-        skill: [{
-            name: "HTML",
-            percent: "90%"
-        },
-            {
-                name: "Css",
-                percent: "90%"
-            },
-            {
-                name: "React",
-                percent: "80%"
-            },
-            {
-                name: "Vue",
-                percent: "60%"
-            },
-            {
-                name: "Redux",
-                percent: "70%"
-            },
-            {
-                name: "dva",
-                percent: "70%"
-            },
-            {
-                name: "JavaScript",
-                percent: "85%"
-            },
-            {
-                name: "ES6",
-                percent: "75%"
-            },
-            {
-                name: "Antd",
-                percent: "75%"
-            },
-            {
-                name: "Git",
-                percent: "70%"
-            }
-        ],
-        usually: [{
-            name: "GitHub",
-            description: "收集代码并写下自己的理解"
-        },
-            {
-                name: "掘金",
-                description: "学习一些技术文章"
-            },
-            {
-                name: "思否 SegmentFault",
-                description: "收藏并学习新的技术"
-            }
-        ],
-        resumeIntroduce: {
-            content: [
-                "本简历基于react组件化开发，",
-                "使用react+ES6+less+webpack，",
-                "数据用云端mock生成，通过fetch请求获得。",
-                "如果您想配置成自己的简历，或者想看源码，",
-                "请点击屏幕右上角按钮访问我的github主页。"
-            ]
-        }
+  data: {
+    baseInfo: {
+      nikeName: "Walter White",
+      firstName: "张",
+      lastName: "凌浩",
+      nameFootNote: "一个Vue,React技术方向的开发者",
+      gender: "男",
+      age: "32",
+      education: "本科",
+      exp: "7年前端开发经验",
+      phone: "19800377291",
+      QQ: "-",
+      // email: '17612235899@163.com',
+      email: "my1900@qq.com",
+      gitHub: "https://github.com/linghaozhang",
+      location: "天津",
     },
-    success: true
-}
+    introduce: {
+      content: [
+        "7年＋的前端开发经验。2年＋的北京React+redux开发经验。参与过内部管理系统开发和电商开发。",
+        "参与产品功能设计，并开发投产",
+        "近期独立带队完成大型智慧交通项目",
+        "擅长通用组件库开发，疑难问题公关",
+        "做过小程序开发",
+        "有做过web APP。",
+        "用jest做过前端单测",
+        "经常使用 antd 和 less，喜欢逛掘金，SegmentFault，github等开发者社区。",
+      ],
+      tech: [
+        "react",
+        "vue",
+        "redux",
+        "react-redux",
+        "dva",
+        "ES6",
+        "less",
+        "antd",
+        "antd-mobile",
+        "webpack",
+        "jquery",
+        "bootStrap",
+        "H5",
+        "git",
+      ],
+    },
+    experience: [
+      {
+        time: "2019-11 至今",
+        title: "北京易华录信息技术股份有限公司",
+        proTitle: "Eloc信控平台",
+        intro: "高级前端开发",
+        description: "主导了前后分离，整体项目重构",
+        tips: [
+          {
+            title: "主要业绩：",
+            content: [
+              "平台重构100%",
+              "开发了核心功能组件",
+              "封装基础工具函数、组件",
+              "疑难问题公关。",
+              "通过严酷测试，如期发布版本",
+            ],
+          },
+        ],
+        technology: [
+          {
+            title: "技术栈：",
+            content: ["vue", "vux", "webpack", "less", "ES6"],
+          },
+        ],
+      },
+      {
+        time: "",
+        title: "易商阜极科技股份有限公司",
+        proTitle: "SCRM",
+        intro: "中级前端开发",
+        description: "主要负责搭建基于vue的h5开发环境。",
+        tips: [
+          {
+            title: "主要业绩：",
+            content: [
+              "使用vue和vux组件库搭建移动端开发环境。",
+              "封装基础工具函数、组件",
+              "组内成员培训",
+            ],
+          },
+        ],
+        technology: [
+          {
+            title: "技术栈：",
+            content: ["vue", "vux", "webpack", "less", "ES6"],
+          },
+        ],
+      },
+      {
+        time: "",
+        title: "易商阜极科技股份有限公司",
+        proTitle: "信美电商积分兑换系统",
+        intro: "中级前端开发",
+        description:
+          "主要负责基于react的Pc和webApp的前端开发，系统支持内部积分兑换，支付宝购买等流程。",
+        tips: [
+          {
+            title: "主要业绩：",
+            content: [
+              "重构积分兑换代码，组件化开发，维护性提高。",
+              "兑换流程的优化。前后端分离。",
+              "成员技术分享。",
+            ],
+          },
+        ],
+        technology: [
+          {
+            title: "技术栈：",
+            content: [
+              "react",
+              "react-redux",
+              "antd",
+              "antd-mobile",
+              "webpack",
+              "less",
+              "ES6",
+            ],
+          },
+        ],
+      },
+      {
+        time: "",
+        title: "易商阜极科技股份有限公司",
+        proTitle: "信美保险产品",
+        intro: "中级前端开发",
+        description:
+          "主要负责基于react的信美保险产品的开发,主要流程包括险种价格试算，投被保人信息，健康告知，face++人脸识别。",
+        tips: [
+          {
+            title: "主要业绩：",
+            content: [
+              "开发了包括，共创配置中心，海外医疗，高端医疗，交通意外，短期意外等险种",
+              "优化了配置中心的数据模型，使共创中心实现了多渠道对接。",
+              "同时做了app开发",
+            ],
+          },
+        ],
+        technology: [
+          {
+            title: "技术栈：",
+            content: [
+              "react",
+              "react-redux",
+              "antd",
+              "antd-mobile",
+              "webpack",
+              "less",
+              "ES6",
+            ],
+          },
+        ],
+      },
+      {
+        time: "",
+        title: "易商阜极科技股份有限公司",
+        proTitle: "标准工程",
+        intro: "中级前端开发",
+        description: "主要负责工程内常用方法、组件的封装",
+        tips: [
+          {
+            title: "主要业绩：",
+            content: [
+              "期间封装了基于fetch的异步请求方法",
+              "基于antd2.x的表单组件封装",
+              "参与了使用标准工程开发的外部bp系统，企业管理部分",
+            ],
+          },
+        ],
+        technology: [
+          {
+            title: "技术栈：",
+            content: [
+              "react",
+              "react-redux",
+              "antd",
+              "antd-mobile",
+              "webpack",
+              "less",
+              "ES6",
+            ],
+          },
+        ],
+      },
+      {
+        time: "",
+        title: "易商阜极科技股份有限公司",
+        proTitle: "电商保全项目",
+        intro: "中级前端开发",
+        description: "该项目基于已承保的用户，进行保全申请操作",
+        tips: [
+          {
+            title: "主要业绩：",
+            content: [
+              "完成了贷款清偿，减保保全项目",
+              "使用了face++人脸识别，对用户进行身份鉴定",
+              "参与了清偿需求分析，与后端共同制定报文",
+            ],
+          },
+        ],
+        technology: [
+          {
+            title: "技术栈：",
+            content: [
+              "react",
+              "react-redux",
+              "antd",
+              "antd-mobile",
+              "webpack",
+              "less",
+              "ES6",
+            ],
+          },
+        ],
+      },
+    ],
+    skill: [
+      {
+        name: "HTML",
+        percent: "90%",
+      },
+      {
+        name: "Css",
+        percent: "90%",
+      },
+      {
+        name: "React",
+        percent: "80%",
+      },
+      {
+        name: "Vue",
+        percent: "60%",
+      },
+      {
+        name: "Redux",
+        percent: "70%",
+      },
+      {
+        name: "dva",
+        percent: "70%",
+      },
+      {
+        name: "JavaScript",
+        percent: "85%",
+      },
+      {
+        name: "ES6",
+        percent: "75%",
+      },
+      {
+        name: "Antd",
+        percent: "75%",
+      },
+      {
+        name: "Git",
+        percent: "70%",
+      },
+    ],
+    usually: [
+      {
+        name: "GitHub",
+        description: "收集代码并写下自己的理解",
+      },
+      {
+        name: "掘金",
+        description: "学习一些技术文章",
+      },
+      {
+        name: "思否 SegmentFault",
+        description: "收藏并学习新的技术",
+      },
+    ],
+    resumeIntroduce: {
+      content: [
+        "本简历基于react组件化开发，",
+        "使用react+ES6+less+webpack，",
+        "数据用云端mock生成，通过fetch请求获得。",
+        "如果您想配置成自己的简历，或者想看源码，",
+        "请点击屏幕右上角按钮访问我的github主页。",
+      ],
+    },
+  },
+  success: true,
+};
